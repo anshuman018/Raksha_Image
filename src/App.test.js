4d5ff3dca3ea5a859f0cb63471e403eb337ee70b
Anshuman <findmeansh@gmail.com>
2025-04-02 16:50:36 +0530

    test: add new test cases for app components

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Raksha/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('renders security badge', () => {
+  render(<App />);
+  const securityElement = screen.getByText(/BETA/i);
+  expect(securityElement).toBeInTheDocument();
+});
+
+test('renders all tabs', () => {
+  render(<App />);
+  const encryptTab = screen.getByText(/Encrypt/i);
+  const decryptTab = screen.getByText(/Decrypt/i);
+  const securityTab = screen.getByText(/Security/i);
+  
+  expect(encryptTab).toBeInTheDocument();
+  expect(decryptTab).toBeInTheDocument();
+  expect(securityTab).toBeInTheDocument();
 });
